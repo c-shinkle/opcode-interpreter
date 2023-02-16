e27db55968dcd1e941ad57a3d7520acd19c020ef
Christian Shinkle <cshinkle2018@gmail.com>
2023-02-15 23:01:51 -0600

    fixed bug with capacity

--- src/opcode/stringify.rs
+++ src/opcode/stringify.rs
@@ -7,6 +7,7 @@ pub fn itertools_join(codes: &[i32]) -> String {
 }
 
 pub fn precompute_capacity(codes: &[i32]) -> String {
+    assert!(!codes.is_empty(), "slice is not empty!");
     let mut chars = String::with_capacity(calc_capacity(codes));
     let mut codes_iter = codes.iter();
     write!(&mut chars, "{}", codes_iter.next().unwrap()).unwrap();
@@ -23,8 +24,8 @@ fn calc_capacity(codes: &[i32]) -> usize {
         if is_negative {
             val *= -1;
         }
-        acc + (is_negative as usize + (val.checked_ilog10().unwrap_or(0) + 1) as usize)
-    })
+        acc + (is_negative as usize + (val.checked_ilog10().unwrap_or(0) + 1) as usize) + 1
+    }) - 1
 }
 
 #[cfg(test)]
@@ -34,24 +35,24 @@ mod tests {
     #[test]
     fn calc_capacity_1_2_3() {
         let actual = calc_capacity(&[1, 2, 3]);
-        assert_eq!(actual, 3);
+        assert_eq!(actual, "1,2,3".len());
     }
 
     #[test]
     fn calc_capacity_10_20_30() {
         let actual = calc_capacity(&[10, 20, 30]);
-        assert_eq!(actual, 6);
+        assert_eq!(actual, "10,20,30".len());
     }
 
     #[test]
     fn calc_capacity_neg_1_2_3() {
         let actual = calc_capacity(&[-1, -2, -3]);
-        assert_eq!(actual, 6);
+        assert_eq!(actual, "-1,-2,-3".len());
     }
 
     #[test]
     fn calc_capacity_neg_1_0_1() {
         let actual = calc_capacity(&[-1, 0, 1]);
-        assert_eq!(actual, 4);
+        assert_eq!(actual, "-1,0,1".len());
     }
 }
